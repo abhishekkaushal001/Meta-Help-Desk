7719ca04e867b11b90531e0df2bd069392615d27
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-21 22:54:36 +0530

    Middleware set up completed with Edge runtime.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="h-screen flex place-items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl">
+        <h2 className="font-bold px-12 mt-3">Facebook Page Integration</h2>
+        <button className="btn btn-primary px-8 w-full mt-10">
+          <Link href="/">Connect Page</Link>
+        </button>
+      </div>
+    </div>
+  );
 }
---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("helpdesk-access-token");
+
+  if (!cookie)
+    return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
+
+  const validate = await fetch(
+    "https://edge-middleware-jwt-authentication-sable.vercel.app/api/protected",
+    {
+      headers: { Cookie: `user-token=${cookie.value}` },
+    }
+  );
+
+  console.log(validate.status);
+
+  if (validate.status !== 200) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
+  }
+}
+
+export const config = {
+  matcher: ["/"],
+};
