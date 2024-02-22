b10a38db474b0922d71342aa72dc973af0df2b71
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-23 02:07:19 +0530

    Fixed deployment bugs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../utils/authOptions";
 import axios from "axios";
 import { IoIosCloudCircle } from "react-icons/io";
 import { GoStack } from "react-icons/go";
