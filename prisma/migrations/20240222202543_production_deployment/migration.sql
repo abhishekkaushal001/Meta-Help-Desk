089b36fc8178b468dd73695a4e236252a8d62696
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-23 01:56:30 +0530

    AWS RDS connected.

--- prisma/migrations/20240222202543_production_deployment/migration.sql
+++ prisma/migrations/20240222202543_production_deployment/migration.sql
@@ -1,4 +1,15 @@
 -- CreateTable
+CREATE TABLE `AppUser` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `AppUser_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Account` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
