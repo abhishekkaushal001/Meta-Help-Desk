80341bf9cbb9ce0da7701f22bc2e6779e5fed11b
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-23 11:02:50 +0530

    Callback error fixed.

--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -9,8 +9,6 @@ export const authOptions: NextAuthOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-      authorization:
-        "https://www.facebook.com/v11.0/dialog/oauth?client_id=2058177371209814&config_id=932801108430824&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Ffacebook&state=NbgoptpJDEDmA3ZELtX7sm5Nyiegv1oxCxKM3hUAX0Q",
     }),
   ],
   session: {
