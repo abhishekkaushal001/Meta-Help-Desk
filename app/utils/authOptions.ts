450148a49f27a18e5770c00cec9782da452da753
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-23 11:26:09 +0530

    Auth URI updated.

--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -9,6 +9,8 @@ export const authOptions: NextAuthOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+      authorization:
+        "https://www.facebook.com/v19.0/dialog/oauth?client_id=2058177371209814&config_id=932801108430824&response_type=code&redirect_uri=https%3A%2F%2Fmeta-help-desk.vercel.app%2Fapi%2Fauth%2Fcallback%2Ffacebook&state=KE_7gIvBd3RHtyTcNBHsThVTMn-9rL5yOief0v4SN9k",
     }),
   ],
   session: {
