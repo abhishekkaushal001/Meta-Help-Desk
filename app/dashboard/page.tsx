c34a61d940f3a98ee100164484b015fba71c20b2
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-24 22:06:07 +0530

    Props fixed.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../utils/authOptions";
 import { getData } from "../webhook/getData";
 import Dashboard from "./Dashboard";
+import prisma from "@/prisma/client";
 
 export interface PageDataType {
   data: [
@@ -35,9 +36,18 @@ const AgentDashboardPage = async () => {
     redirect("/login");
   }
 
-  const page = await getData();
+  const page = await prisma.pageData.findMany({
+    where: {
+      userEmail: session.user?.email!,
+    },
+  });
 
-  return <Dashboard page={page!} />;
+  let pageData;
+  if (page.length === 0) {
+    pageData = await getData();
+  }
+
+  return <Dashboard page={page.length === 0 ? pageData! : page[0]} />;
 };
 
 export default AgentDashboardPage;
